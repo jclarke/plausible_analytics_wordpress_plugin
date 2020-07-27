b2e445d129f1e410707fd3c097f5b964db8a3c8b
Joseph Clarke <jclarke@hey.com>
2020-07-27 09:50:13 -0700

    Inital Commit of Version 1.0

---
+++ plausible_unofficial.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @package Plausible Analytics Unofficial Plugin
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Plausible Analytics Unofficial Plugin
+Plugin URI: https://blog.taskbill.io/plausible-analytics-plugin/
+Description: This plugin adds the Plausible Analytics script to your wordpress site and can optionally not send analytics data for logged in wordpress admin users.
+Author: TaskBill.io
+Version: 1.0.0
+Author URI: http://taskbill.io
+*/
+
+
+add_action( 'wp_head', 'plausible_unofficial_add_snippet');
+
+
+function plausible_unofficial_add_snippet() {
+	$host = get_option("plausible_unofficial_domain_name");
+	$disabled_admin_tracking =  get_option("plausible_unofficial_disable_admin_analytics");
+	
+	if ( ($disabled_admin_tracking && !current_user_can( 'administrator' ) ) ||  !$disabled_admin_tracking ) {
+  	  echo "<script async defer data-domain='" . $host . "' src='https://plausible.io/js/plausible.js'></script>";	
+	}
+}
+
+
+
+// Add an option to save the custom url
+function  plausible_unofficial_register_settings() {
+   $default_host = parse_url(site_url())["host"];
+   add_option( 'plausible_unofficial_domain_name',$default_host );
+   add_option( 'plausible_unofficial_disable_admin_analytics');
+      
+   register_setting( 'plausible_unofficial_options_group', 'plausible_unofficial_domain_name' );
+   register_setting( 'plausible_unofficial_options_group', 'plausible_unofficial_disable_admin_analytics' );
+
+}
+add_action( 'admin_init', 'plausible_unofficial_register_settings' );
+
+// Add the option to the setting in Wordpress
+function plausible_unofficial_register_options_page() {
+  add_options_page('Plausible Analytics Unofficial Plugin', 'Plausible Analytics Settings', 'manage_options', 'plausible_unofficial', 'plausible_unofficial_options_page');
+}
+add_action('admin_menu', 'plausible_unofficial_register_options_page');
+
+function plausible_unofficial_options_page(){
+?>
+  <div>
+  <?php screen_icon(); ?>
+  <h2>Plausible Analytics Unofficial Plugin by <a href="https://TaskBill.io/?utm_source=plausible_plugin">TaskBill.io</a></h2>
+  <p>Please verify your domain matches the domain you have entered in Plausible Analytics. <br>You can also disable tracking of any user that is logged in as an administrator by checking the Disable Tracking of Admin Users below.</p>
+  
+  <form method="post" action="options.php">
+  <?php settings_fields( 'plausible_unofficial_options_group' ); ?>
+  <table>
+  <tr valign="top">
+  <th scope="row"><label for="plausible_unofficial_domain_name">Domain</label></th>
+  <td><input type="text" id="plausible_unofficial_domain_name" name="plausible_unofficial_domain_name" value="<?php echo get_option("plausible_unofficial_domain_name"); ?>" /></td>
+  </tr>
+  <tr valign="top">
+  <th scope="row"><label for="plausible_unofficial_disable_admin_analytics">Disable Tracking of Admin Users</label></th>
+  <td><input type="checkbox" id="plausible_unofficial_disable_admin_analytics" name="plausible_unofficial_disable_admin_analytics" value='1'  <?php echo checked(1, get_option("plausible_unofficial_disable_admin_analytics"),false); ?> />  
+  </td>
+  </tr>
+  </table>
+  <?php  submit_button(); ?>
+  </form>
+  </div>
+<?php
+	}
---
+++ readme.txt
@@ -0,0 +1,18 @@
+=== Plausible Analytics Unofficial Plugin ===
+Contributors: joemclarke
+Tags: plausible, analytics
+Requires at least: 5.4
+Tested up to: 5.4.2
+Requires PHP: 5.6.20
+Stable tag: trunk
+License: GPLv2
+
+This plugin adds the Plausible Analytics tracking code to the header of your wordpress blog.
+
+== Description ==
+This plugin automatically adds the Plausible Analytics javascript code to the header of your wordpress blog. You can optionally change the tracking domain in the settings page. You can also optionally disable tracking of logged in admin users.
+
+== Installation ==
+Upload the plausible_unofficial folder to wp-content/plugins
+
+Activate the plugin and verify your domain is correct in Settings->Plausible Analytics Settings.
